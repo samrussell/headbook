d59eb89dadc1562c63decca96385a73aebf2162b
Sam Russell <sam.h.russell@gmail.com>
2016-09-27 20:44:58 +1300

    basic post infrastructure

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,19 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.most_recent_first.first(5)
   end
 
   def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create!(params[:post].permit(:title, :subtitle, :body))
+
+    redirect_to @post
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ActiveRecord::Base
+  scope :most_recent_first, -> { order(:created_at => :desc) }
 end

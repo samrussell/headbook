06b87e76d2ea0fc076bb429186b6244b5ba6e481
Sam Russell <sam.h.russell@gmail.com>
2016-09-28 23:09:17 +1300

    second cuke

--- features/new_post.feature
+++ features/new_post.feature
@@ -5,6 +5,18 @@ Feature: New posts
     And I type "srsly its good" in the "Subtitle" field
     And I type "this is the content" in the "Write your post" field
     And I click the "Save" button
+    Then I should be redirected to my new post
 
     When I go to the post index page
     Then I should see the post titled "best post in the world"
+
+  Scenario: Only show 5 posts on the first page
+    Given there are 5 posts
+    When I go to the post index page
+    Then I should see links to the last 5 posts
+    And I should see a link to the first post
+
+    When I create a new post
+    And I go to the post index page
+    Then I should see links to the last 5 posts
+    And I should not see a link to the first post
--- features/step_definitions/post_steps.rb
+++ features/step_definitions/post_steps.rb
@@ -19,3 +19,46 @@ Then(/^I should see the post titled "([^"]*)"$/) do |post_title|
   link_texts = links.map { |link| link.text }
   expect(link_texts).to include(post_title)
 end
+
+Then(/^I should be redirected to my new post$/) do
+  expect(page).to have_current_path(post_path(Post.last.id))
+end
+
+Given(/^there are (\d+) posts$/) do |post_count|
+  post_count.to_i.times do
+    step %{I create a new post}
+  end
+end
+
+Then(/^I should see links to the last (\d+) posts$/) do |post_count|
+  recent_posts = Post.last(post_count.to_i)
+  links = all(:css, "a")
+  link_urls = links.map { |link| link["href"] }
+
+  recent_posts.each do |post|
+    expect(link_urls).to include(post_path(post))
+  end
+end
+
+When(/^I create a new post$/) do
+  steps %{
+    When I go to the new post page
+    And I type "best post in the world" in the "Post title" field
+    And I type "srsly its good" in the "Subtitle" field
+    And I type "this is the content" in the "Write your post" field
+    And I click the "Save" button
+  }
+end
+
+Then(/^I should( not)? see a link to the first post$/) do |modifier|
+  first_post = Post.first
+
+  links = all(:css, "a")
+  link_urls = links.map { |link| link["href"] }
+
+  if modifier
+    expect(link_urls).to_not include(post_path(first_post))
+  else
+    expect(link_urls).to include(post_path(first_post))
+  end
+end
